2ee875c623b55bef3c4503ef4f9b65e7f0174409
AJ <ajoeford@gmail.com>
2017-08-10 14:48:13 -0700

    pre routes

---
+++ src/containers/ParkContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ParkContainer extends React.Component {
+
+  render() {
+    return <div>
+      <h1>Park Details blah blah</h1>
+    </div>
+  }
+}
+
+export default ParkContainer;
--- todo.md
+++ todo.md
@@ -1,6 +1,7 @@
 High
 - Add additional info on popups
 - move mapbox token out of js
+- add individual park page
 
 Moderate
 - Change map marker icon
