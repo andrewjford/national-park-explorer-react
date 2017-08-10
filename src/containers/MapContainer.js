71646c48a1759db56a2f9d4121c140ed3e15efcf
AJ <ajoeford@gmail.com>
2017-08-09 18:55:42 -0700

    leaflet map setup

---
+++ src/containers/MapContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+
+export default class MapContainer extends React.Component {
+  state = {
+    lat: 37.0902,
+    lng: -95.7129,
+    zoom: 4,
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </span>
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
+}
