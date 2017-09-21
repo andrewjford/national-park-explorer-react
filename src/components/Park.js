4290c76221695f4bf59d144dbc5f640783c35e38
AJ <ajoeford@gmail.com>
2017-09-21 09:34:13 -0700

    Park view renders properly if API is up

--- src/components/Park.js
+++ src/components/Park.js
@@ -3,23 +3,7 @@ import ConditionalImg from './ConditionalImg';
 
 const Park = (props) => {
 
-  if(props.park.id) {
-    // render view from local db park data
-    return <div className="park-container">
-      <h1>{props.park.fullName}</h1>
-
-      <p>{props.park.description}</p>
-
-      <h3>Weather</h3>
-      <p>{props.park.weatherInfo}</p>
-      <p className="park-error">The National Park Service Data API appears to be down. For further detailed information on the park please visit the park webpage below or try again later.</p>
-      <span>Official Website: <a href={props.park.url}>{props.park.url}</a></span>
-      <br/>
-      <span>Directions: <a href={props.park.directionsUrl}>{props.park.directionsUrl}</a></span>
-    </div>
-  }
-
-  else {
+  if(props.park.db_id) {
     // render view from NPS API park data
     return <div className="park-container">
       <h1>{props.park.fullName}</h1>
@@ -34,6 +18,22 @@ const Park = (props) => {
 
       <h3>Weather</h3>
       <p>{props.park.weatherInfo}</p>
+      <span>Official Website: <a href={props.park.url}>{props.park.url}</a></span>
+      <br/>
+      <span>Directions: <a href={props.park.directionsUrl}>{props.park.directionsUrl}</a></span>
+    </div>
+  }
+
+  else {
+    // render view from local db park data
+    return <div className="park-container">
+      <h1>{props.park.fullName}</h1>
+
+      <p>{props.park.description}</p>
+
+      <h3>Weather</h3>
+      <p>{props.park.weatherInfo}</p>
+      <p className="park-error">The National Park Service Data API appears to be down. For further detailed information on the park please visit the park webpage below or try again later.</p>
       <span>Official Website: <a href={props.park.url}>{props.park.url}</a></span>
       <br/>
       <span>Directions: <a href={props.park.directionsUrl}>{props.park.directionsUrl}</a></span>
